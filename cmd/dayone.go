89e26a40f5af5bee8e6a8e810fe49d98532b502f
Jeremy Smitherman <jeremysmitherman@gmail.com>
2022-12-02 15:03:33 -0600

    Added day one task

---
+++ cmd/dayone.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"aoc2022/dayone"
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"sync"
+)
+
+func main() {
+	// Ensure we get a file name as the argument
+	if len(os.Args) != 2 {
+		fmt.Printf("Usage: ./dayone [INPUT FILE]")
+		os.Exit(1)
+	}
+
+	// Open the input file, close the handle on exit
+	f, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatalf("File not found: %s\n", os.Args[1])
+	}
+	defer f.Close()
+
+	// Create our sorter and counter builder closure.  Create a waitgroup for the goroutines
+	calorieSorter := dayone.CounterSorter{}
+	buildCalorieCounter := dayone.NewCalorieCounter()
+	wg := &sync.WaitGroup{}
+
+	// Build a scanner to read our input file easily and set it to split on newlines
+	scanner := bufio.NewScanner(f)
+	scanner.Split(bufio.ScanLines)
+
+	// Build the initial calorie counter and have it start listening in a goroutine
+	calorieCounter := buildCalorieCounter()
+	go calorieCounter.Listen(wg)
+
+	for {
+		for scanner.Scan() {
+			input := scanner.Text()
+			// Blank line, so close the input channel on the current counter, start a goroutine to insert it into the
+			// calorie sorter asynchronously, then create a new calorie counter and continue scanning
+			if input == "" {
+				close(calorieCounter.Input)
+				go calorieSorter.Insert(calorieCounter, wg)
+
+				calorieCounter = buildCalorieCounter()
+				go calorieCounter.Listen(wg)
+				continue
+			} else {
+				// Value found on this line, convert the input, send it to the input channel of the current calorie counter
+				val, err := strconv.Atoi(input)
+				if err != nil {
+					log.Fatalf(err.Error())
+				}
+				calorieCounter.Input <- val
+			}
+		}
+
+		// End of the file.  Close the current counter, and asynchronously send it to the sorter then end the loop.
+		close(calorieCounter.Input)
+		go calorieSorter.Insert(calorieCounter, wg)
+		break
+	}
+
+	// Wait for all the counters and the sorter to finish, Get the last entry of the sorter's ordered list and print the
+	// calorie total it contains.
+	wg.Wait()
+	fmt.Println(calorieSorter.OrderedList[len(calorieSorter.OrderedList)-1].Total)
+}
---
+++ dayone/calorieCounter.go
@@ -0,0 +1,76 @@
+package dayone
+
+import "sync"
+
+// CalorieCounter contains a channel to take input, and ID to identify the struct, and the total calorie count
+type CalorieCounter struct {
+	Input chan int
+	ID    int
+	Total int
+}
+
+// Listen takes an int input and adds it to the total until the Input channel is closed
+func (c *CalorieCounter) Listen(wg *sync.WaitGroup) {
+	wg.Add(1)
+	for {
+		v, open := <-c.Input
+		if !open {
+			wg.Done()
+			break
+		} else {
+			c.Total += v
+		}
+	}
+}
+
+// NewCalorieCounter returns a function that will give you a new Calorie counter with a unique ID
+func NewCalorieCounter() func() *CalorieCounter {
+	nextID := 0
+	return func() *CalorieCounter {
+		nextID += 1
+		return &CalorieCounter{
+			Input: make(chan int),
+			ID:    nextID,
+		}
+	}
+}
+
+// CounterSorter contains an ordered list of CalorieCounter sorted by the total calories
+type CounterSorter struct {
+	OrderedList []*CalorieCounter
+	lock        *sync.Mutex
+}
+
+// Insert takes a counter and inserts it in order of Total calories
+func (c *CounterSorter) Insert(counter *CalorieCounter, waitGroup *sync.WaitGroup) {
+	if c.lock == nil {
+		c.lock = &sync.Mutex{}
+	}
+
+	// Add to the waitgroup and lock the mutex
+	waitGroup.Add(1)
+	c.lock.Lock()
+
+	// Clear locks and inform WG that we're finished.
+	defer func() {
+		waitGroup.Done()
+		c.lock.Unlock()
+	}()
+
+	// If we're the biggest known total, just add it to the end and call it a day
+	if len(c.OrderedList) == 0 || counter.Total > c.OrderedList[len(c.OrderedList)-1].Total {
+		c.OrderedList = append(c.OrderedList, counter)
+		return
+	}
+
+	// I love Go sometimes
+	var newList []*CalorieCounter
+	for _, currentCounter := range c.OrderedList {
+		if counter.Total <= currentCounter.Total {
+			newList = append(newList, currentCounter)
+			continue
+		}
+		newList = append(newList, currentCounter)
+	}
+	c.OrderedList = newList
+}
